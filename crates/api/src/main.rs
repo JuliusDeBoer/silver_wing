8b04a134b04139532b8fa3d3a15e88be02354a46
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-08-09 00:51:32 +0200

    feat: Add API functionality

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,10 +1,12 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello from the API!"
-}
+use cuid2::cuid;
+use rocket::serde::json::Json;
+use shared::Entry;
+use std::sync::Mutex;
+
+static QUEUE: Mutex<Vec<Entry>> = Mutex::new(Vec::new());
 
 #[get("/kitty")]
 fn kitty() -> &'static str {
@@ -22,7 +24,30 @@ fn kitty() -> &'static str {
               `--'   `--'"
 }
 
+#[post("/push", data = "<entry>")]
+fn push(entry: &str) {
+    let binding = &QUEUE;
+    let mut queue = binding.lock().expect("Could not get lock");
+    queue.push(Entry {
+        id: cuid(),
+        body: entry.into(),
+    });
+}
+
+#[get("/entries")]
+fn get_entries() -> Json<Vec<shared::Entry>> {
+    let queue = QUEUE.lock().expect("Could not get lock");
+    Json(queue.clone())
+}
+
+#[put("/clear", data = "<entries>")]
+fn clear(entries: Json<Vec<String>>) {
+    let binding = &QUEUE;
+    let mut queue = binding.lock().expect("Could not get lock");
+    queue.retain(|entry| !entries.contains(&entry.id));
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, kitty])
+    rocket::build().mount("/", routes![push, get_entries, clear, kitty])
 }
--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -1,6 +1,12 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
 pub struct HealthStatus {
     pub healthy: bool,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Entry {
+    pub id: String,
+    pub body: String,
 }
